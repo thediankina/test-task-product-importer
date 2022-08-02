cfa74c529cc0eb90e1386c1f610a79446ab65c19
Diana <digaliulina@gmail.com>
2022-08-02 16:28:25 +0500

    Create migrations

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * Контроллер для управления товарами
+ */
+class ProductController extends Controller
+{
+    /**
+     * Выполнить импорт товаров
+     *
+     * @return void
+     */
+    public function import(): void
+    {
+
+    }
+}
---
+++ database/migrations/2022_08_02_091956_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Запуск миграции
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('code', 15);
+            $table->decimal('price');
+            $table->string('preview_text', 30);
+            $table->text('detail_text');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Откат миграции
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
